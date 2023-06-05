80a1f7cceb6260f1c89850c1b12d9c7cd822ad5f
Newton Lomar <newtonlomar16@gmail.com>
2023-06-05 01:09:25 -0300

    definindo dados do usuário

--- test/users/users.spec.ts
+++ test/users/users.spec.ts
@@ -1,11 +1,31 @@
 import test from 'japa'
 import supertest from 'supertest'
-
+/*
+{
+  "users": {
+    "id":       number,
+    "email":    string,
+    "username": string,
+    "password": string,
+    "avatar":   string
+  }
+}
+*/
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
 test.group('User', () => {
   test.only('it should create an user', async (assert) => {
-    const userPayload = { email: 'test@test.com', username: 'test', password: 'test' }
-
-    await supertest(BASE_URL).post('/users').send(userPayload).expect(201)
+    const userPayload = {
+      email: 'test@test.com',
+      username: 'test',
+      password: 'test',
+      avatar: 'https://avatars.githubusercontent.com/u/40671286?v=4',
+    }
+    const { body } = await supertest(BASE_URL).post('/users').send(userPayload).expect(201)
+    assert.exists(body.user, 'User undefined')
+    assert.exists(body.user.id, 'Id undefined')
+    assert.equal(body.user.email, userPayload.email)
+    assert.equal(body.user.username, userPayload.username)
+    assert.equal(body.user.id, userPayload.password)
+    assert.equal(body.user.avatar, userPayload.avatar)
   })
 })
