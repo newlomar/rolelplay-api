23f4629937b8e7be0983fcdc9f806fad448fd959
Newton Lomar <newtonlomar16@gmail.com>
2023-06-05 09:24:00 -0300

    usando model hooks para criptografar dados sensíveis

--- test/users/users.spec.ts
+++ test/users/users.spec.ts
@@ -21,11 +21,14 @@ test.group('User', () => {
       avatar: 'https://avatars.githubusercontent.com/u/40671286?v=4',
     }
     const { body } = await supertest(BASE_URL).post('/users').send(userPayload).expect(201)
+
+    console.log(body)
+
     assert.exists(body.user, 'User undefined')
     assert.exists(body.user.id, 'Id undefined')
     assert.equal(body.user.email, userPayload.email)
     assert.equal(body.user.username, userPayload.username)
-    assert.equal(body.user.password, userPayload.password)
+    assert.notExists(body.user.password, 'Password defined')
     assert.equal(body.user.avatar, userPayload.avatar)
   })
 })
