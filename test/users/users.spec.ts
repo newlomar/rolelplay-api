7c131fec916471d7024f91de14df3b67ec1915a7
Newton Lomar <newtonlomar16@gmail.com>
2023-06-06 16:55:34 -0300

    usando factories e criando teste para criação de usuário com email ja em uso

--- test/users/users.spec.ts
+++ test/users/users.spec.ts
@@ -1,3 +1,4 @@
+import { UserFactory } from 'Database/factories'
 import test from 'japa'
 import supertest from 'supertest'
 /*
@@ -13,7 +14,7 @@ import supertest from 'supertest'
 */
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
 test.group('User', () => {
-  test.only('it should create an user', async (assert) => {
+  test('it should create an user', async (assert) => {
     const userPayload = {
       email: 'test@test.com',
       username: 'test',
@@ -30,5 +31,17 @@ test.group('User', () => {
     assert.equal(body.user.username, userPayload.username)
     assert.notExists(body.user.password, 'Password defined')
     assert.equal(body.user.avatar, userPayload.avatar)
+  })
+
+  test('it should return 409 when email is already in use', async (assert) => {
+    const { email } = await UserFactory.create()
+    const { body } = await supertest(BASE_URL)
+      .post('/users')
+      .send({
+        email,
+        username: 'test',
+        password: 'test',
+      })
+      .expect(409)
   })
 })
